00d7e4724514d3a8c524e72d49dcbbb55c798026
jakelare <jakelare@gmail.com>
2015-12-07 16:26:34 -0500

    J.P. Jennings December 7th 4:25PM Latest Model

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,17 +1,14 @@
 class SiteController < ApplicationController
-	def welcome
-		render("welcome")
-	end
 	def about
 		render("about")
 	end
 	def services
 		render("services")
 	end
-	def news
-		render("news")
-	end
 	def contact
 		render("contact")
+	end
+	def signup
+		render("signup")
 	end
 end
