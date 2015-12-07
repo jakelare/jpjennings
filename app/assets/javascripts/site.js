ee43a13cc0b138235a15efb71f2c56bf3af0a5e5
jakelare <jakelare@gmail.com>
2015-12-07 12:01:16 -0500

    December 6th J.P. Jennings 12:00PM Single Page Menu Bar Update

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,2 +1,12 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(document).ready(function(){
+    var elem = $('.navbar');
+    $(window).scroll(function(){
+        if ($(window).scrollTop() > 300){
+            elem.show();
+        } else if ($(window).scrollTop() < 300){
+            elem.hide();
+        }
+    });
+});
